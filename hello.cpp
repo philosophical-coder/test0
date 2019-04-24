4280080f1709d44f3ef0c8f92cd1b7f5e9e1ee20
philosophical-coder <philosophical.coder11@gmail.com>
2019-04-24 19:38:49 +0600

    Added comments

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
+// This file is to test git commands
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
     cout << "hello world, this is my first code pushed on github" << endl;
+    return 0;
 }
